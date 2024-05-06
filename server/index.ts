8520179908cb1eaf4bab0429cd09adb8e488c7b9
Esmin53 <esmin.tufekcic53@gmail.com>
2024-05-06 14:47:05 +0200

    first commit

---
+++ server/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(3000, () => console.log("A SUIIIIee"))
